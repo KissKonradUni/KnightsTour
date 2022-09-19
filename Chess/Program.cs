ffc39ec4823dfd86292f585a2dd5baeeb6000a85
Kiss Konrád Soma <kisskonraduni@proton.me>
2022-09-19 14:17:45 +0200

    Added multiple try solutions.

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -18,6 +18,8 @@ internal static class Constants
     public const float NeighbourToWholeWeight    = 0.80f;
     public const float NeighbourToWholeThreshold = 0.95f;
     public const int   NeighbourToWholeCycles    = 2;
+
+    public const float VeryStuckMultiplier       = 0.25f;
 }
 
 internal class Position
@@ -47,9 +49,19 @@ internal class Position
     {
         return MathF.Sqrt(X*X + Y*Y);
     }
+
+    public bool Equals(Position obj)
+    {
+        return obj.X == X && obj.Y == Y;
+    }
+
+    public Position Copy()
+    {
+        return new Position(X, Y);
+    }
 }
 
-class Table
+internal class Table : ICloneable
 {
     private readonly bool[,]  _travelMap;
     private readonly int[,]   _stepMap;
@@ -58,6 +70,8 @@ class Table
     private Position _playerPosition;
     private readonly Position _tableSize;
     private readonly float    _maxDistance;
+
+    private readonly List<Position> _loweredPriorityPlaces;
 
     public bool[,]  TravelMap      => _travelMap;
     public int[,]   StepMap        => _stepMap;
@@ -74,6 +88,20 @@ class Table
         }
     }
 
+    public Table(Position tableSize, Position playerStart, bool[,] travelMap,
+        float[,] priorityMap, int[,] stepMap)
+    {
+        _travelMap      = travelMap.Clone()   as bool[,]  ?? throw new InvalidOperationException();
+        _priorityMap    = priorityMap.Clone() as float[,] ?? throw new InvalidOperationException();
+        _stepMap        = stepMap.Clone()     as int[,]   ?? throw new InvalidOperationException();
+
+        _tableSize      = tableSize;
+        _maxDistance    = tableSize.Length();
+        _playerPosition = playerStart;
+        
+        _loweredPriorityPlaces = new List<Position>();
+    }
+    
     public Table(Position tableSize, Position playerStart)
     {
         _travelMap      = new bool [tableSize.X, tableSize.Y];
@@ -82,10 +110,24 @@ class Table
         _tableSize      = tableSize;
         _maxDistance    = tableSize.Length();
         _playerPosition = playerStart;
+        
+        _loweredPriorityPlaces = new List<Position>();
 
         UpdateMaps();
     }
 
+    public void ResetPriority()
+    {
+        _loweredPriorityPlaces.Clear();
+        GeneratePriorityMap();
+    }
+    
+    public void LowerPriority(Position value)
+    {
+        _loweredPriorityPlaces.Add(value);
+        GeneratePriorityMap();
+    }
+    
     private bool CanMoveThere(Position position)
     {
         return !(
@@ -201,6 +243,11 @@ class Table
                 }
             }
         }
+        
+        foreach (var lowered in _loweredPriorityPlaces)
+        {
+            _priorityMap[lowered.X, lowered.Y] *= Constants.VeryStuckMultiplier;
+        }
     }
 
     public Position? GetMostDesirableStep()
@@ -232,6 +279,11 @@ class Table
     {
         return _stepMap.Cast<int>().Max() != 0;
     }
+
+    public object Clone()
+    {
+        return MemberwiseClone();
+    }
 }
 
 public static class Program
@@ -240,21 +292,23 @@ public static class Program
     {
         Console.Clear();
         
+        Console.WriteLine($"{_runCount}. Try");
+        
         for (var x = 0; x < table.TableSize.X; x++)
         {
             for (var y = 0; y < table.TableSize.Y; y++)
             {
-                Console.SetCursorPosition(x * 2, y);
+                Console.SetCursorPosition(x * 2, y + 1);
                 
                 if (table.PlayerPosition.X != x || table.PlayerPosition.Y != y)
                     Console.Write($"\x1b[0m{(table.TravelMap[x, y] ? "\x1b[38;5;203mX" : "\x1b[38;5;4m-")}\x1b[38;5;255m|");
                 else
                     Console.Write("\x1b[38;5;50m%\x1b[38;5;255m|");
                 
-                Console.SetCursorPosition(table.TableSize.X * 2 + 2 + x * 3, y);
+                Console.SetCursorPosition(table.TableSize.X * 2 + 2 + x * 3, y + 1);
                 Console.Write($"\x1b[38;5;3m{table.StepMap[x,y].ToString().PadLeft(2, '0')}\x1b[38;5;255m|");
                 
-                Console.SetCursorPosition(table.TableSize.X * 5 + 4 + x * 5, y);
+                Console.SetCursorPosition(table.TableSize.X * 5 + 4 + x * 5, y + 1);
                 if (table.PlayerPosition.X != x || table.PlayerPosition.Y != y)
                     Console.Write($"{(table.PriorityMap[x,y] > 0.5f ? (table.PriorityMap[x,y] > 0.75f ? (table.PriorityMap[x,y] > 0.9f ? "\x1b[38;5;4m" : "\x1b[38;5;48m") : "\x1b[38;5;228m") : "\x1b[38;5;203m")}{table.PriorityMap[x,y]:0.00}\x1b[38;5;255m|");
                 else
@@ -263,32 +317,105 @@ public static class Program
             }
         }
     }
+
+    private static Position _tableSize = new(8, 8);
+    private static Position _playerStart = new(3, 3);
+        
+    private static Table _table = new(_tableSize, _playerStart);
+    private static Position _lastStep = new(0,0);
     
+    private static int _runCount = 1;
+    private static bool _hidden = false;
+    
+    private static readonly List<Table> History = new();
+    private static readonly List<Position> StepHistory = new();
+
     public static int Main(string[] args)
     {
-        var table = new Table(new Position(8, 8), new Position(3, 3));
+        Console.Write("\n\nChess ");
+        foreach (var s in args)
+        {
+            Console.Write($"{s} ");
+            if (s.StartsWith("-t"))
+            {
+                var format = s.Replace("-t", "").Split(",");
+                _tableSize = new Position(int.Parse(format[0]), int.Parse(format[1]));
+            } else if (s.StartsWith("-p"))
+            {
+                var format = s.Replace("-p", "").Split(",");
+                _playerStart = new Position(int.Parse(format[0]), int.Parse(format[1]));
+            } else if (s.StartsWith("-h"))
+            {
+                _hidden = true;
+            }
+        }
+        _table = new Table(_tableSize, _playerStart);
+        Console.WriteLine();
+        Console.ReadLine();
 
         do
         {
-            if (table.IsDone())
-                Console.WriteLine("DONE");
-            
-            UpdateConsole(table);
+            UpdateConsole(_table);
 
             Console.ReadLine();
 
-            var next = table.GetMostDesirableStep();
+            var next = _table.GetMostDesirableStep();
             if (next != null)
             {
-                table.PlayerPosition = next;
+                History.Add(new Table(_table.TableSize, _table.PlayerPosition, _table.TravelMap, _table.PriorityMap, _table.StepMap));
+
+                if (_lastStep.Equals(next))
+                {
+                    RevertStep();
+                    Console.WriteLine("Very stuck, sorry!");
+                    Console.ReadLine();
+                    var thisStep = _table.PlayerPosition.Copy();
+                    _table = History[0];
+                    _runCount++;
+
+                    for (var i = StepHistory.Count / 2; i < StepHistory.Count; i++)
+                    {
+                        _table.LowerPriority(StepHistory[i]);    
+                    }
+                    
+                    _table.LowerPriority(thisStep);
+                    History.Clear();
+                }
+                else
+                {
+                    _table.PlayerPosition = next;
+                    StepHistory.Add(next);
+                    _lastStep = next;   
+                }
             }
             else
             {
-                Console.WriteLine(table.IsStuck() ? "Stuck!" : "Done!");
+                if (_table.IsStuck())
+                {   
+                    Console.WriteLine("Stuck!");
+                    Console.ReadLine();
 
-                Console.ReadLine();
-                return 0;
+                    RevertStep();
+                }
+                else
+                {
+                    Console.WriteLine("Done!");
+                    Console.ReadLine();
+                    
+                    return 0;
+                }
             }
         } while (true);
+    }
+
+    private static void RevertStep()
+    {
+        var lastIndex = History.Count - 1;
+        _table = History[lastIndex];
+        History.RemoveAt(lastIndex);
+        
+        var lastStepIndex = StepHistory.Count - 1;
+        _table.LowerPriority(StepHistory[lastStepIndex]);
+        StepHistory.RemoveAt(lastStepIndex);
     }
 }
